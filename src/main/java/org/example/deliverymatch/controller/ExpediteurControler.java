189e360f942e8bd463aa83a018cda7649f0621de
ridaDev <kiratterida@gmail.com>
2025-06-17 16:25:54 +0100

    créer methode GetAllExpediteur

--- src/main/java/org/example/deliverymatch/controller/ExpediteurControler.java
+++ src/main/java/org/example/deliverymatch/controller/ExpediteurControler.java
@@ -6,10 +6,9 @@ import org.example.deliverymatch.entity.Expediteur;
 import org.example.deliverymatch.service.ConducteurService;
 import org.example.deliverymatch.service.ExpediteurService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -23,9 +22,17 @@ public class ExpediteurControler {
         this.expediteurService = expediteurService;
     }
 
+
+    // ajouter expediteur
     @PostMapping("/addExpediteur")
     public Expediteur addExpediteur(@RequestBody Expediteur expediteur) {
         return expediteurService.addExpediteur(expediteur);
+    }
+
+    // afficher touts expediteur
+    @GetMapping("/getAllExpediteur")
+    public List<Expediteur> getExpediteurs() {
+        return expediteurService.getExpediteurs();
     }
 
 
--- src/main/java/org/example/deliverymatch/service/ExpediteurService.java
+++ src/main/java/org/example/deliverymatch/service/ExpediteurService.java
@@ -6,6 +6,8 @@ import org.example.deliverymatch.repository.ExpediteurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ExpediteurService {
 
@@ -15,8 +17,13 @@ public class ExpediteurService {
     public ExpediteurService(ExpediteurRepository expediteurRepository) {
         this.expediteurRepository = expediteurRepository;
     }
-
+// ajouter expediteur
     public Expediteur addExpediteur(Expediteur expediteur) {
         return expediteurRepository.save(expediteur);
+    }
+
+    //afficher tout expediteur
+    public List<Expediteur> getExpediteurs() {
+        return expediteurRepository.findAll();
     }
 }
