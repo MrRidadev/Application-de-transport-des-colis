d766782f0c1a0614301bc7963c1599cd8bee5a3b
ridaDev <kiratterida@gmail.com>
2025-06-17 17:22:01 +0100

    créer methode createDemande

--- src/main/java/org/example/deliverymatch/controller/DemandeController.java
+++ src/main/java/org/example/deliverymatch/controller/DemandeController.java
@@ -1,4 +1,30 @@
 package org.example.deliverymatch.controller;
 
+
+import org.example.deliverymatch.entity.Demande;
+import org.example.deliverymatch.repository.ExpediteurRepository;
+import org.example.deliverymatch.service.DemandeService;
+import org.example.deliverymatch.service.ExpediteurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Demande")
 public class DemandeController {
+
+    private final DemandeService demandeService;
+
+    @Autowired
+    public DemandeController(DemandeService demandeService) {
+        this.demandeService = demandeService;
+    }
+
+    // ajouter demande
+    @PostMapping("/addDemande")
+    public Demande addDemande(@RequestBody Demande demande) {
+        return demandeService.addDemande(demande);
+    }
 }
---
+++ src/main/java/org/example/deliverymatch/service/DemandeService.java
@@ -0,0 +1,24 @@
+package org.example.deliverymatch.service;
+
+import org.example.deliverymatch.entity.Demande;
+import org.example.deliverymatch.entity.Expediteur;
+import org.example.deliverymatch.repository.DemandeRepository;
+import org.example.deliverymatch.repository.ExpediteurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemandeService {
+
+    private final DemandeRepository demandeRepository;
+    @Autowired
+    public DemandeService(DemandeRepository demandeRepository) {
+        this.demandeRepository = demandeRepository;
+    }
+
+    // ajouter demande
+    public Demande addDemande(Demande demande) {
+        return demandeRepository.save(demande);
+    }
+
+}
